ac9544896d486c928208aba241a27624535b888a
Joan Killeen <joan.killeen@bluewin.ch>
2021-12-13 16:29:34 +0100

    Finished Tic Tac Toe

---
+++ PE1/Module_4/LAB_4.7.2.1.py
@@ -0,0 +1,133 @@
+import random
+LINE_START = "+-------+-------+-------+"
+LINE_CELL_PADDING = "|       |       |       |"
+CELL_START = "|   "
+CELL_END = "   "
+MOVE_BY_COMPUTER = 'X'
+MOVE_BY_USER = 'O'
+BEST_POSITION=(1,1)
+board = []
+
+
+def init_board():
+    
+    for x in range(0,3):
+        board.append([])
+        for y in range(0,3):           
+            board[x].append((3*x) + y + 1)
+        print(board[x])
+
+def display_board(board):
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+    
+    for x in range(len(board)):
+        print(LINE_START)
+        print(LINE_CELL_PADDING)
+        new_line=''
+        for y in range(len(board[0])):
+            new_line = new_line + CELL_START + str(board[x][y]) + CELL_END
+        new_line = new_line + CELL_START
+        print(new_line)
+        print(LINE_CELL_PADDING)
+    print(LINE_START)
+    
+
+
+def enter_move(board):
+    free_fields = make_list_of_free_fields(board)   
+    move = int(input("Input your next move in the range 1 to 9: "))
+    if move not in range(1,10):
+        print("Not a valid move!")
+        return
+    for tup in free_fields:        
+        if move == board[tup[0]][tup[1]]:
+            board[tup[0]][tup[1]] = MOVE_BY_USER
+            return
+            
+
+def make_list_of_free_fields(board):    
+    # The function browses the board and builds a list of all the free squares; 
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    free_fields = []
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            if board[x][y] != MOVE_BY_USER and board[x][y] != MOVE_BY_COMPUTER:
+                free_fields.append((x,y))
+    return free_fields
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+    
+    # Check rows
+    x=0
+    y=0
+    for x in range(len(board)):
+        victory_row = True
+        for y in range(len(board[0])):
+            victory_row = victory_row and board[x][y] == sign
+        if victory_row:
+            return True
+    print("have checked rows ok")
+    x=0
+    y=0
+    #Check columns
+    for y in range(len(board[0])):
+        victory_column = True
+        for x in range(len(board)):
+           victory_column = victory_column and board[x][y] == sign
+        if victory_column:
+            return True
+    
+    x=0
+    y=0
+    #Check diagonals
+    victory_first_diag = True
+    for x in range(len(board)):
+        victory_first_diag = victory_first_diag and board[x][x] == sign
+    if victory_first_diag:
+        return True
+
+    
+    y=0
+    #Check second diagonal
+    victory_second_diag = True
+    for y in range(len(board[0])):
+        x=2-y
+        victory_second_diag = victory_second_diag and board[x][y] == sign
+    if victory_second_diag:
+        return True
+
+    return False
+                   
+
+def draw_move(board):
+    tup=()
+    free_fields = make_list_of_free_fields(board)
+    if BEST_POSITION in free_fields:
+        tup=(1,1)
+    else:
+        tup = random.choice(free_fields)
+    board[tup[0]][tup[1]] = MOVE_BY_COMPUTER
+
+init_board()
+free_fields = make_list_of_free_fields(board)
+victory = False
+draw_move(board)
+while free_fields != [] and victory==False:
+    display_board(board)
+    print(free_fields)
+    enter_move(board)
+    victory = victory_for(board, MOVE_BY_USER)
+    if (victory):
+        print("Victory for: ", "YOU", "!")
+        break
+    free_fields = make_list_of_free_fields(board)
+    if free_fields != []:
+        draw_move(board)
+    victory = victory_for(board, MOVE_BY_COMPUTER)
+    if (victory):
+        print("Victory for: ", "COMPUTER", "!")
+        break
+
